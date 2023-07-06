7f373b9d1aefda08412a8200ff3ddd475924a2de
Leonardo Prestes <methoddbr@gmail.com>
2023-07-06 19:14:11 -0300

    feat: create auth route

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Trips from "./componentes/Trips";
+
+export const metadata = {
+    title: "About"
+}
 
 const Page = () => {
-    return <div>page</div>;
+    return (
+    <div>
+        <Trips />
+    </div>
+    )
 };
 
 export default Page;
